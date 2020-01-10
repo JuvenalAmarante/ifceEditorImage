e680b158fe702b280f18fbe7d4a410a9b819c225
Renan Roseno <rosenorenan@gmail.com>
2020-01-10 12:58:40 -0300

    Criação do codigo fonte / Criação da estrutura

---
+++ codigo.c
@@ -0,0 +1,17 @@
+// -------> Bibliotecas
+#include <stdio.h>
+
+// -------> Dimensões da imagem
+#define ALTURA 0
+#define LARGURA 0
+
+//-------> Definisão das funções 
+void init();
+void girarHorizontal();
+void girarVertical();
+void filtro();
+
+
+int main(void) 
+  return 0;
+}
