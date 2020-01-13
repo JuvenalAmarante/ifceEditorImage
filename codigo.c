dc55f0fe32943c1850f30009b310d78be012cd0b
Renan Roseno <rosenorenan@gmail.com>
2020-01-13 15:13:13 -0300

    Funções init e copiar implementadas.
    Tratamento de exceção na criação dos arquivos e o função de copiar imagem realizada.

--- codigo.c
+++ codigo.c
@@ -1,17 +1,61 @@
 // -------> Bibliotecas
 #include <stdio.h>
+#include <stdlib.h>
+
 
 // -------> Dimensões da imagem
 #define ALTURA 0
 #define LARGURA 0
 
+// Nome dos arquivos 
+#define FOTO "lenna.png"
+#define COPIA "len.png"
+
 //-------> Definisão das funções 
-void init();
+void init(FILE *img1);
+void copiarImg(FILE *img1);
+
+void rotacionar90H();
+void rotacionar90AH();
 void girarHorizontal();
 void girarVertical();
 void filtro();
 
-
-int main(void) 
+//----------> Função main
+int main(void){
+  FILE *img1;
+  img1 = fopen(FOTO, "r+");
+  
+  init(img1);
+  copiarImg(img1);
+  
+  fclose(img1);
+  
   return 0;
+}
+
+//-----> Função para verificar erros na inicialização
+
+void init(FILE *img1){
+  if(img1 == NULL){
+    printf("Erro ao iniciar arquivo");
+    exit(0);
+  }
+}
+
+
+// Função copiar imagem 
+void copiarImg(FILE *img1){
+//------> Arquivo c
+  FILE *img2 = fopen(COPIA, "w+");
+  char aux;
+//-------> Cursor para o inicio do arquivo
+  rewind(img1); 
+//-------> Copiando caracteres 
+  while(!(feof(img1))){
+    aux = getc(img1); 
+       fputc(aux,img2);
+  }
+//----> Fechando arquivo
+  fclose(img2);
 }
